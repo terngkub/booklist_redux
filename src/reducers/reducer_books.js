81db75357a560a9f26c8b0e12dc5aa5c487f32bb
terngkub <terngnattapol@gmail.com>
2018-03-30 12:56:44 +0200

    create book list

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+		{ title: 'JavaScript: The Good Parts' },
+		{ title: 'Harray Potter' },
+		{ title: 'The Dark Tower' },
+		{ title: 'Eloquent Ruby' }
+	];
+}
